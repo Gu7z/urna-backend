5885756eb95f7412392696fddcf072ca4f58d5d8
Gu7z <gustavoferri13@gmail.com>
2019-12-26 21:46:00 -0400

    Mostrando votos computados

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ var peopleSchema = new mongoose.Schema({
     image: String
 });
 
+var voteSchema = new mongoose.Schema({
+    vote: String
+});
+
+
 var People = mongoose.model('people', peopleSchema);
+var Vote = mongoose.model('vote', voteSchema);
 
 app.use(bodyParser.json());
 app.use(cors())
@@ -33,7 +39,7 @@ app.get('/', async (req, res)=>{
 app.post('/novo', async (req, res)=>{
     var salvo = false
     var people = new People(req.body)
-    var alo = await people.save()
+    await people.save()
     .then(
         item => {
             salvo = true
@@ -41,7 +47,7 @@ app.post('/novo', async (req, res)=>{
     ).catch(
         err => {
             salvo = false
-            res.status(400).send("unable to save to database")
+            res.status(400).send("Nao foi possivel enviar o dado")
         }
     )
     if (salvo){
@@ -50,6 +56,71 @@ app.post('/novo', async (req, res)=>{
         res.send('Não Enviado')
     }
     res.end()
+})
+
+app.post('/vote', async (req, res) => {
+    console.log(req.body)
+    var salvo = false;
+    var vote = new Vote(req.body)
+    await vote.save()
+    .then(
+        data => salvo = true
+    )
+    .catch(
+        err => {salvo = false; res.status(400).send("Nao foi possivel computar o voto")}
+    )
+
+    if ( salvo ){
+        res.send('Computado')
+    }else{
+        res.send('Nao enviado')
+    }
+
+    res.end()
+})
+
+app.get('/contar', async(req, res)=>{
+    var finalResult = [];
+    var isDone = false;
+    var getcountObj
+    
+    Vote.aggregate(
+        [
+            {
+                $group: 
+                {
+                    _id: "$vote", count: {$sum: 1}
+                }
+            }
+        ]
+    ).then( async (qr) => { 
+
+        var results = qr.map( async (countObj) => {
+
+            await People.find({_id: countObj._id}, {name: 1, _id: 0}).then( (qrry) => {
+                
+                qrry.map( pessoaObj => {
+                    result = {
+                        name: pessoaObj.name,
+                        votes: countObj.count
+                    }
+
+                    finalResult = result
+                } )
+
+            } )
+
+            return(finalResult);
+
+        } )
+
+        Promise.all(results).then((completed) => {
+            res.send(completed)
+        })
+
+    } )
+
+    
 })
 
 app.listen(port, ()=> console.log(`Rodando na porta: ${port}`))
